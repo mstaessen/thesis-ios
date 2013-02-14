437d665c98ba18022ea565323ef5369dddb6d3de
Bert Outtier <outtierbert@gmail.com>
2013-02-14 09:14:59 +0100

    Initial Commit

---
+++ ExpenseApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExpenseApp
+//
+//  Created by Bert Outtier on 14/02/13.
+//  Copyright (c) 2013 Bert Outtier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
